53285f3102cd9f3bbf7e0a5166ed9fe9344f2744
migue <miguel-gutierrez@mail.de>
2023-04-07 14:08:26 +0200

    auth with loggedin state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Router }            from '@angular/router';
+import { Observable }        from 'rxjs';
+import { map }               from 'rxjs/operators';
 // @ts-ignore
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid }        from 'uuid';
 
-import { LoginState } from './feature/auth/share/model/LoginState';
+import { LoginState }                  from './feature/auth/share/model/LoginState';
 import { LoginStateCollectionService } from './feature/auth/share/store/login-state-collection.service';
-import { ToastMessage } from './feature/shared/model/ToastMessage';
-import { ToastMessageCollectionServiceService } from './feature/shared/service/toast-message-collection-service.service';
+import { ToastMessage }                from './feature/shared/model/ToastMessage';
+import {
+  ToastMessageCollectionServiceService
+}                                      from './feature/shared/service/toast-message-collection-service.service';
 
 const initialState: LoginState = {
   id: uuid(),
   isLoggedIn: false,
   success: false,
   token: null
-}
+};
 
-@Component({
+@Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+  styleUrls: [ './app.component.scss' ]
+} )
 export class AppComponent implements OnInit {
   title = 'JuliasHaushaltsSystem';
   isImpressOpen = false;
@@ -33,24 +35,24 @@ export class AppComponent implements OnInit {
     private toastMessageCollectionServiceService: ToastMessageCollectionServiceService,
     private loginStateCollectionService: LoginStateCollectionService
   ) {
-    const storedLoginState: string | null = localStorage.getItem('loginState');
-    const loginState = storedLoginState ? {...JSON.parse(storedLoginState), id: uuid()} : initialState;
-    this.loginStateCollectionService.addOneToCache(loginState);
+    const storedLoginState: string | null = localStorage.getItem( 'loginState' );
+    const loginState = storedLoginState ? { ...JSON.parse( storedLoginState ), id: uuid() } : initialState;
+    this.loginStateCollectionService.addOneToCache( loginState );
     this.loginState$ = this.loginStateCollectionService.entities$.pipe(
-      map(states => states[0])
+      map( states => states[ 0 ] )
     );
   }
 
   ngOnInit(): void {
     this.toastMessage$ = this.toastMessageCollectionServiceService.entities$.pipe(
-      map(toastMessages => toastMessages[0])
+      map( toastMessages => toastMessages[ 0 ] )
     );
   }
 
   onLogout(): void {
     this.loginStateCollectionService.clearCache();
     localStorage.clear();
-    this.loginStateCollectionService.addOneToCache(initialState);
-    this.router.navigate(['/login']);
+    this.loginStateCollectionService.addOneToCache( initialState );
+    this.router.navigate( [ '/login' ] );
   }
 }
